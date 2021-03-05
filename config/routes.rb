d212e1c96d6fabc0952b2e09552df641c962e9b9
GenryZ <77881488+GenryZ@users.noreply.github.com>
2021-03-05 20:49:43 +0300

    Add pages Terms and About in blog.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
   resource :contacts, only: [:new, :create], path_names: {:new => ''} #only это хэш, new и create то что хотим оставить
                                             #delete, put, update  не будут создаваться
                                             #эти маршруты routes
